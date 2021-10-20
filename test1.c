9e8b01454916ec73c7dcb87cd1760eec863cb1b3
Shiv Chveli <shivchevli9212@gmail.com>
2021-10-20 21:03:00 +0530

    Add Binary File in Repository And Make the next commit as Ammend for the previous commit which is already pushed on remote Repository.

---
+++ test1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+    int n1, n2;
+    printf("Hello World!\n");
+    printf("Enter Numeber 1 :- ");
+    scanf("%d", &n1);
+    printf("Enter Numeber 2 :- ");
+    scanf("%d", &n2);
+
+    printf("Sum Of number %d and %d :- %d", n1, n2, n1 + n2);
+}
