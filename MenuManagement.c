6478c916ff7020d4088da776b5554b19e8adac03
Shiv Chveli <shivchevli9212@gmail.com>
2021-11-12 23:52:11 +0530

    Completed Write back to File and Add Fuctionality of Adding New MenueItem

--- MenuManagement.c
+++ MenuManagement.c
@@ -2,28 +2,13 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <string.h>
-/*
-Repeted File
- */
-typedef struct BUINDINGNODE
-{
-    int number;
-    char bindValue[20];
-    struct BUINDINGNODE *next;
-} Bind;
-
-typedef struct BINDHEADE
-{
-    Bind *userType;
-    Bind *categoryType;
-} MapHeadNode;
 
 typedef struct MENUE
 {
     int id;
     int category;
     int price;
-    char name[20];
+    char name[40];
     struct MENUE *next;
 } MenuItem;
 
@@ -31,32 +16,83 @@ typedef struct MENUE
 typedef struct MENUEHEADER
 {
     int categoryNumber;
-    char category[20];
+    int NumberOfItem;
+    char category[40];
     struct MENUE *items;
     struct MENUEHEADER *next;
 } MenueHeader;
 // End New Item
 
+// typedef struct HeadTraker
+// {
+//     int maxId;
+//     int NumberofCetogory;
+//     MenueHeader *head;
+// } ;
+
 void PrintHeading(char str[])
 {
-    printf("\n===========================================================\n");
-    printf("\t\t%s \n", str);
-    printf("===========================================================\n");
+    printf("\n======================================================================================\n");
+    printf("\t\t\t\t%s ", str);
+    printf("\n=======================================================================================\n");
 };
 
 void PrintError(char str[])
 {
-    printf("\n##########################################################\n");
+    printf("\n########################################################################################\n");
     printf("Error :- ");
-    printf("\n##########################################################\n");
+    printf("\n########################################################################################\n");
     printf("%s", str);
-    printf("\n-----------------------------------------------------------\n");
+    printf("\n----------------------------------------------------------------------------------------\n");
 }
 
 void LogMark()
 {
-    printf("\n-----------------------------------------------------------\n");
+    printf("\n----------------------------------------------------------------------------------------\n");
 };
+
+void setLength(char *str, int len)
+{
+    int j = 0;
+    while (str[j] != '\0')
+    {
+        j++;
+    }
+    while (j < len - 1)
+    {
+        str[j] = ' ';
+        j++;
+    }
+    str[j] = '\0';
+}
+
+void setItemNode(MenueHeader *HeadTraker, MenuItem *HeadPointer)
+{
+
+    MenuItem *trakItem;
+    /*
+    Put nodes in Category Link list
+    */
+    while (HeadTraker->categoryNumber != HeadPointer->category)
+    {
+        HeadTraker = HeadTraker->next;
+    }
+
+    if (HeadTraker->items == NULL)
+    {
+        HeadTraker->items = HeadPointer;
+    }
+    else
+    {
+
+        trakItem = HeadTraker->items;
+        while (trakItem->next != NULL)
+        {
+            trakItem = trakItem->next;
+        }
+        trakItem->next = HeadPointer;
+    }
+}
 
 MenueHeader *getMenueHeadersFromFile(char file[])
 {
@@ -107,6 +143,7 @@ MenueHeader *getMenueHeadersFromFile(char file[])
             if (column == 1)
             {
                 strcpy(menueCategory->category, value);
+                setLength(menueCategory->category, 40);
             }
 
             value = strtok(NULL, ", ");
@@ -120,9 +157,6 @@ MenueHeader *getMenueHeadersFromFile(char file[])
     return head;
 };
 
-/*
-New Added Item starts
- */
 void *getMenueItemFromFile(char file[], MenueHeader *head)
 {
 
@@ -180,6 +214,7 @@ void *getMenueItemFromFile(char file[], MenueHeader *head)
             if (column == 2)
             {
                 strcpy(HeadPointer->name, value);
+                setLength(HeadPointer->name, 40);
                 // printf("\t\tUser Name :");
             }
 
@@ -195,29 +230,7 @@ void *getMenueItemFromFile(char file[], MenueHeader *head)
         }
 
         printf("%d\t%d\t%s\t%d\n", HeadPointer->id, HeadPointer->category, HeadPointer->name, HeadPointer->price);
-        /*
-            Put nodes in Category Link list
-        */
-        while (HeadTraker->categoryNumber != HeadPointer->category)
-        {
-            HeadTraker = HeadTraker->next;
-        }
-
-        if (HeadTraker->items == NULL)
-        {
-            HeadTraker->items = HeadPointer;
-        }
-        else
-        {
-
-            trakItem = HeadTraker->items;
-            while (trakItem->next != NULL)
-            {
-                trakItem = trakItem->next;
-            }
-            trakItem->next = HeadPointer;
-        }
-
+        setItemNode(head, HeadPointer);
         // printf("\n");
     }
     fclose(fp);
@@ -234,7 +247,7 @@ void DisplayMenue(MenueHeader *start)
         PrintHeading(start->category);
         track = start->items;
         LogMark();
-        printf("| ItemId\t| ItemName\t| ItemPrice");
+        printf("| ItemId\t| ItemName\t\t\t\t\t| ItemPrice");
         LogMark();
         while (track != NULL)
         {
@@ -246,10 +259,79 @@ void DisplayMenue(MenueHeader *start)
     printf("Exit");
 }
 
+void DisplayCetegory(MenueHeader *head)
+{
+
+    PrintHeading("Menue Cetegory");
+    LogMark();
+    printf("| CategoryID\t\t| CetorogyName");
+    LogMark();
+    while (head != NULL)
+    {
+        printf("| %d\t\t| %s\n", head->categoryNumber, head->category);
+        head = head->next;
+    }
+};
+
+void WriteDataToFile(MenueHeader *node, char *file1, char *file2)
+{
+
+    int column = 0, row = 0;
+    char buffer[1024];
+    char pre[] = "./Database/";
+    char pre1[] = "./Database/";
+    FILE *fp1 = fopen(strcat(pre, file1), "w");
+    FILE *fp2 = fopen(strcat(pre1, file2), "w");
+    fprintf(fp1, "number,CategoryName\n");
+    fprintf(fp2, "ItemId,ItemCetogaory,ItemName,Price\n");
+    MenuItem *temp = NULL;
+    while (node != NULL)
+    {
+        fprintf(fp1, "%d,%s\n", node->categoryNumber, node->category);
+        temp = node->items;
+        while (temp != NULL)
+        {
+            fprintf(fp2, "%d,%d,%s,%d\n", temp->id, temp->category, temp->name, temp->price);
+            temp = temp->next;
+        }
+        node = node->next;
+    }
+
+    fclose(fp1);
+    fclose(fp2);
+}
+/*
+New Added Item starts
+ */
+
+MenuItem *createMenueItem(MenueHeader *head)
+{
+    MenuItem *new = (MenuItem *)malloc(sizeof(MenuItem));
+    DisplayCetegory(head);
+    printf("select Cetogry :- ");
+    scanf("%d", &new->category);
+
+    printf("Enter Item Name :- ");
+    gets(new->name);
+    gets(new->name);
+    setLength(new->name, 40);
+    printf("Enter Item Price :- ");
+    scanf("%d", &new->price);
+    new->next = NULL;
+    new->id = -1;
+
+    return new;
+};
+
 void main()
 {
     MenueHeader *start = getMenueHeadersFromFile("CategoryType.csv");
+    MenueHeader *traker = NULL;
     getMenueItemFromFile("MenueItem.csv", start);
     DisplayMenue(start);
     printf("Pregram Runs");
+    MenuItem *new = createMenueItem(start);
+    setItemNode(start, new);
+    DisplayMenue(start);
+    WriteDataToFile(start, "CategoryType.csv", "MenueItem.csv");
 }
